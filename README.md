037b63216ea6b1866ad4525653072dedc96be14b
Matt Faulkner <mnfaulk@gmail.com>
2015-02-27 19:43:18 -0800

    publishing and consuming test messages using kafka-quickstart

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Angular-websocket
 
-Expands upon the [AngularJS Socket.IO Seed](https://github.com/btford/angular-socket-io-seed) demo by adding Docker support and a Fleet unit file.
+Expands upon the [AngularJS Socket.IO Seed](https://github.com/btford/angular-socket-io-seed) demo by adding Docker support, a Fleet unit file, and Kafka integration.
 
 ## Running locally
 ```
@@ -11,12 +11,12 @@ And navigate to `localhost:3000`
 
 ## Running locally with Docker
 ```
-docker build -t angular-websocket .
-docker run -p 3000:3000 -d angular-websocket
+docker build -t angular-websocket-kafka .
+docker run -p 3000:3000 -d angular-websocket-kafka
 ```
 The app should now be running on `localhost:3000`, or if you are using boot2docker on port 3000 of your boot2docker IP address (run `echo $(boot2docker ip)` to find it).
 
 ## Deploying to CoreOS
 ```
-fleetctl start angular-websocket.service
+fleetctl start angular-websocket-kafka.service
 ```
--- app.js
+++ app.js
@@ -12,6 +12,47 @@ var app = module.exports = express.createServer();
 // Hook Socket.io into Express
 var io = require('socket.io').listen(app);
 
+// Kafka
+// TODO: get Kafka broker(s) IP from environment variable or etcd
+// This may raise an exception when a new topic is created...
+var KAFKA_BROKER = '192.168.86.5:2181';
+var KAFKA_CLIENT_ID = "angular-websocket-kafka";
+var kafka = require('kafka-node'),
+    Producer = kafka.Producer,
+    Consumer = kafka.Consumer,
+    kafkaClient = new kafka.Client(KAFKA_BROKER, KAFKA_CLIENT_ID),
+    kafkaProducer = new Producer(kafkaClient),
+    kafkaConsumer = new Consumer(
+        kafkaClient,
+        [], // no payloads
+        {
+            autoCommit: false
+        }
+    );
+
+// send a message on a topic
+var payloads = [
+        { topic: 'topic1', messages: 'hi', partition: 0 },
+        { topic: 'topic2', messages: ['hello', 'world'] }
+    ];
+kafkaProducer.on('ready', function () {
+    kafkaProducer.send(payloads, function (err, data) {
+        console.log("producer");
+        console.log(data);
+    });
+
+});
+// console.log the received message
+kafkaConsumer.addTopics(['topic1', 'topic2'], function (err, added) {
+  console.log("consumer");
+  console.log(err);
+  console.log(added);
+});
+
+kafkaConsumer.on('message', function (message) {
+    console.log(message);
+});
+
 // Configuration
 
 app.configure(function(){
